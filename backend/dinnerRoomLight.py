35966785ba713b72ee6d1eb165a6d9882afa8a21
JAbelP <59974707+JAbelP@users.noreply.github.com>
2023-03-02 13:33:46 -0500

    Update dinnerRoomLight.py

--- backend/dinnerRoomLight.py
+++ backend/dinnerRoomLight.py
@@ -1,7 +1,7 @@
 
 import tinytuya
 print("d being set up")
-d = tinytuya.BulbDevice('eb96216e45ecbd9093p4qn','192.168.1.229','9fa92d5b218dcebb')
+d = tinytuya.BulbDevice('XXXXXXXXXXXXX','XXX.XXX.X.xxx','XXXXXXXXXXXXXXX')
 d.set_version(3.3)
 b =d.status()
 print("Status set now")
